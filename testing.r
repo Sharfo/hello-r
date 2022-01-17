94d81ce304143503eb359ff40d183fe75a4eb44d
M.Osama <usa353642@gmail.com>
2022-01-17 14:18:57 +0500

    purpose is added to join this plarteform

--- testing.r
+++ testing.r
@@ -4,3 +4,5 @@ print("hello i am sharfo can you guide me in this plateform ?")
 name = "sharfo"
 print(name)
 print("hello i sharfo :)")
+#why i am is here ?
+print("well i am here to learn data science")
